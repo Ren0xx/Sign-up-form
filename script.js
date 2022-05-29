55b9352031b34028c77f8f9682a31ee8883481d0
Adam Załęski <adamzaleski001@wp.pl>
2022-05-29 12:38:12 +0200

    Finished creating project

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('.form-right');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const label = document.querySelector('#hiddenLabel');
+
+function checkPassword() {
+    if (password1.value === password2.value){
+        label.hidden = true;
+        password1.className = "blueLabel"
+        password2.className = "blueLabel"
+    }
+    else {
+        label.hidden = false;
+        password1.className = "redLabel";
+        password2.className = "redLabel";
+    }
+
+}
